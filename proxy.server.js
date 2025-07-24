c6f029c9fd68432b3a307ef886c29f4c2782853d
John Rhuel Laurente <22-1-00109@vsu.edu.ph>
2025-07-25 04:16:02 +0800

    Initial Commit

---
+++ proxy.server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const proxy = require("express-http-proxy");
+
+const app = express();
+const PORT = 8080; // We'll use 8080
+
+const FRONTEND_ORIGIN = "http://localhost:3000";
+const API_SERVICE_URL = "https://ai-tools.rev21labs.com";
+
+// Enable CORS for your frontend
+app.use(cors({ origin: FRONTEND_ORIGIN }));
+
+// Proxy all requests to the target API
+app.use("/", proxy(API_SERVICE_URL));
+g;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Proxy server is running on http://localhost:${PORT}`);
+  console.log(`Forwarding requests to ${API_SERVICE_URL}`);
+});
